fd0c4e47ed7e24bc9b01019e0f0857d68107a1df
Chandak01 <mmukeshiitkgp@gmail.com>
2018-11-08 19:56:36 +0530

    Add files via upload

--- restructure.py
+++ restructure.py
@@ -1,11 +1,8 @@
-# Python3 program to fin maximum
-# cash flow among a set of persons
- 
-# Number of persons(or vertices in graph)
+import csv
+
+#default
 N = 3
- 
-# A utility function that returns
-# index of minimum value in arr[]
+
 def getMin(arr):
      
     minInd = 0
@@ -13,9 +10,7 @@ def getMin(arr):
         if (arr[i] < arr[minInd]):
             minInd = i
     return minInd
- 
-# A utility function that returns
-# index of maximum value in arr[]
+
 def getMax(arr):
  
     maxInd = 0
@@ -23,67 +18,29 @@ def getMax(arr):
         if (arr[i] > arr[maxInd]):
             maxInd = i
     return maxInd
- 
-# A utility function to
-# return minimum of 2 values
+
 def minOf2(x, y):
  
     return x if x < y else y
- 
-# amount[p] indicates the net amount to
-# be credited/debited to/from person 'p'
-# If amount[p] is positive, then i'th 
-# person will amount[i]
-# If amount[p] is negative, then i'th
-# person will give -amount[i]
+
 def minCashFlowRec(amount):
- 
-    # Find the indexes of minimum
-    # and maximum values in amount[]
-    # amount[mxCredit] indicates the maximum
-    # amount to be given(or credited) to any person.
-    # And amount[mxDebit] indicates the maximum amount
-    # to be taken (or debited) from any person.
-    # So if there is a positive value in amount[], 
-    # then there must be a negative value
     mxCredit = getMax(amount)
     mxDebit = getMin(amount)
- 
-    # If both amounts are 0, 
-    # then all amounts are settled
+
     if (amount[mxCredit] == 0 and amount[mxDebit] == 0):
         return 0
- 
-    # Find the minimum of two amounts
+
     min = minOf2(-amount[mxDebit], amount[mxCredit])
     amount[mxCredit] -=min
     amount[mxDebit] += min
- 
-    # If minimum is the maximum amount to be
     print("Person " , mxDebit , " pays " , min
         , " to " , "Person " , mxCredit)
- 
-    # Recur for the amount array. Note that
-    # it is guaranteed that the recursion
-    # would terminate as either amount[mxCredit] 
-    # or amount[mxDebit] becomes 0
     minCashFlowRec(amount)
- 
-# Given a set of persons as graph[] where
-# graph[i][j] indicates the amount that
-# person i needs to pay person j, this
-# function finds and prints the minimum 
-# cash flow to settle all debts.
+
 def minCashFlow(graph):
- 
-    # Create an array amount[],
-    # initialize all value in it as 0.
+
     amount = [0 for i in range(N)]
- 
-    # Calculate the net amount to be paid
-    # to person 'p', and stores it in amount[p].
-    # The value of amount[p] can be calculated by
-    # subtracting debts of 'p' from credits of 'p'
+
     for p in range(N):
         for i in range(N):
             amount[p] += (graph[i][p] - graph[p][i])
@@ -91,11 +48,23 @@ def minCashFlow(graph):
     minCashFlowRec(amount)
      
 # Driver code
- 
-# graph[i][j] indicates the amount
-# that person i needs to pay person j
-graph = [ [0, 1000, 2000],
-          [0, 0, 5000],
-          [0, 0, 0] ]
- 
+
+print('Enter the number of users: ')
+N = int(input())
+
+graph = [[0 for x in range(int(N))] for y in range(int(N))] 
+
+print('Enter number of transactions: ')
+e = int(input())
+while(e):
+	print("enter 'debtor' 'creditor' 'amount' in space separated format")
+	ints = input()
+	x = [int(i) for i in ints.split(" ")]
+	if(len(x)!=3):
+		print("enter 'debtor' 'creditor' 'amount' in space separated format")
+		continue
+	u,v,w = x[0],x[1],x[2]
+	graph[u-1][v-1] = w
+	e = e-1
+
 minCashFlow(graph)
